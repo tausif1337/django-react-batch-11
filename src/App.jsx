c656c59691f8df15fe058acc59398397db102a67
Md. Tausif Hossain <tausif1337@gmail.com>
2026-09-21 23:15:34 +0600

    Add Redux counter state
    This change introduces a Redux Toolkit store and counter slice, wraps the app in a Provider, and updates the main App component to read and dispatch counter state actions. It also moves the Greeting and Username components into a components folder and adds the Redux dependencies needed for the feature.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { createContext } from "react";
-import Greeting from "./Greeting";
-
-export const UserContext = createContext();
+import { useDispatch, useSelector} from "react-redux";
+import { decrement, increment, changeName } from "./features/counter/counterSlice";
+import Greeting from "./components/Greeting";
 
 function App() {
-  const name = "TAUSIF";
+  const dispatch = useDispatch();
+
+  const count = useSelector((state) => state.counter.value);
+  const name = useSelector((state) => state.counter.name);
 
   return (
-    <div>
-      <UserContext.Provider value={name}>
-        <Greeting />
-        
-      </UserContext.Provider>
+    <div className="min-h-svh flex flex-col justify-center items-center ">
+      <h1 className="text-3xl">Value: {count}</h1>
+      <h1 className="text-3xl">Name: {name}</h1>
+      <button
+        onClick={() => dispatch(increment())}
+        className="px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Increment Button
+      </button>
+
+      <button
+        onClick={() => dispatch(decrement())}
+        className="px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Decrement Button
+      </button>
+
+       <button
+        onClick={() => dispatch(changeName())}
+        className="px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        change Name
+      </button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from "./app/store";
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
